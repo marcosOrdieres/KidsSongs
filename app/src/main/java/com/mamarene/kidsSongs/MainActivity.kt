a3d443eb8272de8c46bbb466adfa24f64c894b12
marcosOrdieres <marcosordieres89@gmail.com>
2019-10-23 00:25:20 +0200

    added music first

--- app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
+++ app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.mamarene.kidsSongs
 
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.happy)
+        mediaPlayer?.setOnPreparedListener{
+            //mediaPlayer?.start()
+            println("readyyyyy")
+        }
     }
 }
