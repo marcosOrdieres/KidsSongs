74a37aa303a2110a2a4dfc15694a0e53dd0d5927
Marcos Rodriguez Ordieres <marcosrodriguezordieres@Marcoss-MacBook-Pro.local>
2019-10-23 16:18:36 +0200

    added background buttons etc

--- app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
+++ app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mamarene.kidsSongs
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +11,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mediaPlayer = MediaPlayer.create(this, R.raw.happy)
+
         setContentView(R.layout.activity_main)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.happy)
         mediaPlayer?.setOnPreparedListener{
-            //mediaPlayer?.start()
-            println("readyyyyy")
+            mediaPlayer?.start()
+            println("holaaaaaa")
+        }
+
+        imageButtonFirst.setOnClickListener{
+            println("weeee")
         }
     }
 }
