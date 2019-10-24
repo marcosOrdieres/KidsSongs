6922958156c441509f2b97ed3bdb741d92f0db34
marcosOrdieres <marcosordieres89@gmail.com>
2019-10-24 23:34:25 +0200

    video player working

--- app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
+++ app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
@@ -4,10 +4,22 @@ import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.VideoView
+import android.widget.MediaController
+import android.net.Uri
+import android.widget.ProgressBar
+
+
 
 class MainActivity : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
+
+    private var videoView: VideoView? = null
+    private var mediaController: MediaController? = null
+    private var uri: Uri? = null
+    private var isContinuously = false
+    private var stopPosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,13 +27,43 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        videoView = findViewById(R.id.videoView) as VideoView
+
+
+        mediaController = MediaController(this)
+        mediaController?.setAnchorView(videoView)
+
+        val uriPath = ("android.resource://" + packageName + "/" + R.raw.shark)
+        uri = Uri.parse(uriPath)
+        videoView!!.setVideoURI(uri)
+
+
         mediaPlayer?.setOnPreparedListener{
             mediaPlayer?.start()
-            println("holaaaaaa")
         }
 
+
         imageButtonFirst.setOnClickListener{
-            println("weeee")
+            if (!isContinuously) {
+                isContinuously = true
+                mediaPlayer?.pause()
+                videoView!!.requestFocus()
+                videoView!!.setMediaController(mediaController)
+                if(videoView!!.getCurrentPosition() > 0){
+                    videoView!!.seekTo(stopPosition)
+                    videoView!!.start()
+
+                } else{
+                    videoView!!.start()
+                }
+
+            } else{
+                isContinuously = false
+                videoView!!.pause()
+                stopPosition = videoView!!.getCurrentPosition(); //stopPosition is an int
+                println(videoView!!.getCurrentPosition())
+
+            }
         }
     }
 }
