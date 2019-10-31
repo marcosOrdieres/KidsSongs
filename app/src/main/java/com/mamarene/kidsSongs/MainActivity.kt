7f7efea2ae54e402bf55ebce5a39751f777b795f
Marcos Rodriguez Ordieres <marcosrodriguezordieres@Marcoss-MacBook-Pro.local>
2019-10-31 17:59:44 +0100

    wip

--- app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
+++ app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
@@ -1,39 +1,30 @@
 package com.mamarene.kidsSongs
 
 import android.media.MediaPlayer
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import android.widget.VideoView
-import android.widget.MediaController
-import android.widget.HorizontalScrollView
-
 import android.net.Uri
-import android.view.View
-import android.widget.ProgressBar
-
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.MediaController
+import core.onEndScroll
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
 
-    private var videoView: VideoView? = null
     private var mediaController: MediaController? = null
     private var uri: Uri? = null
     private var isContinuously = false
     private var stopPosition = 0
-    private var horizontalScrollViewComp: HorizontalScrollView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mediaPlayer = MediaPlayer.create(this, R.raw.happy)
 
         setContentView(R.layout.activity_main)
-
-        videoView = findViewById(R.id.videoView) as VideoView
-
-        horizontalScrollViewComp = findViewById(R.id.horizontalScrollView)
 
         mediaController = MediaController(this)
         mediaController?.setAnchorView(videoView)
@@ -47,6 +38,11 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer?.start()
         }
 
+        horizontalScrollView.onEndScroll({
+            button.visibility = VISIBLE
+        }) {
+            button.visibility = GONE
+        }
 
         imageButtonFirst.setOnClickListener{
             if (!isContinuously) {
@@ -54,7 +50,7 @@ class MainActivity : AppCompatActivity() {
                 mediaPlayer?.pause()
                 videoView!!.requestFocus()
                 videoView!!.setMediaController(mediaController)
-                if(videoView!!.getCurrentPosition() > 0){
+                if(videoView!!.currentPosition > 0){
                     videoView!!.seekTo(stopPosition)
                     videoView!!.start()
                     //horizontalScrollViewComp
@@ -66,8 +62,8 @@ class MainActivity : AppCompatActivity() {
             } else{
                 isContinuously = false
                 videoView!!.pause()
-                stopPosition = videoView!!.getCurrentPosition(); //stopPosition is an int
-                println(videoView!!.getCurrentPosition())
+                stopPosition = videoView!!.currentPosition //stopPosition is an int
+                println(videoView!!.currentPosition)
 
             }
         }
