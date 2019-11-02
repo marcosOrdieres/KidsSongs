1cec8acdcacc24592479e6596fd4674f2f76daca
marcosOrdieres <marcosordieres89@gmail.com>
2019-11-02 20:47:26 +0100

    layout finished

--- app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
+++ app/src/main/java/com/mamarene/kidsSongs/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mamarene.kidsSongs
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
@@ -12,6 +13,9 @@ import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.AdListener
+
+import com.google.android.gms.ads.InterstitialAd
 import com.google.android.gms.ads.MobileAds
 import core.onEndScroll
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     private var stopPosition = 0
     private var horizontalScrollViewComp: HorizontalScrollView? = null
     lateinit var adView : AdView
+    private lateinit var mInterstitialAd: InterstitialAd
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,22 +38,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         MobileAds.initialize(this) {}
+
         adView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         adView.loadAd(adRequest)
 
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-3940256099942544/1033173712"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
+
         videoView = findViewById<VideoView>(R.id.videoView)
         adView = findViewById(R.id.adView)
-
         horizontalScrollViewComp = findViewById(R.id.horizontalScrollView)
 
         mediaController = MediaController(this)
         mediaController?.setAnchorView(videoView)
 
-        val uriPath = ("android.resource://" + packageName + "/" + R.raw.shark)
+        val uriPath = ("android.resource://" + packageName + "/" + R.raw.sol)
         uri = Uri.parse(uriPath)
         videoView!!.setVideoURI(uri)
-
 
         mediaPlayer?.setOnPreparedListener{
             mediaPlayer?.start()
@@ -59,39 +67,83 @@ class MainActivity : AppCompatActivity() {
         }
         arrowLeft?.visibility = GONE
 
-
         horizontalScrollView.onEndScroll({
             arrowRight.visibility = VISIBLE
             arrowLeft?.visibility = GONE
         }) {
             arrowRight.visibility = GONE
             arrowLeft.visibility = VISIBLE
-
         }
+        goBack?.visibility = GONE
 
         imageButtonFirst.setOnClickListener{
             if (!isContinuously) {
                 isContinuously = true
-                mediaPlayer?.pause()
+                mediaPlayer?.stop()
                 videoView!!.requestFocus()
                 videoView!!.setMediaController(mediaController)
                 if(videoView!!.currentPosition > 0){
                     videoView!!.seekTo(stopPosition)
                     videoView!!.start()
-                    horizontalScrollViewComp!!.removeAllViews()
-
+                    horizontalScrollViewComp?.visibility = GONE
                 } else{
                     videoView!!.visibility = VISIBLE
                     arrowRight?.visibility = GONE
                     arrowLeft?.visibility = GONE
-                    horizontalScrollViewComp!!.removeAllViews()
+                    horizontalScrollViewComp?.visibility = GONE
+                    adView?.visibility = GONE
                     videoView!!.start()
+                    goBack?.visibility = VISIBLE
                 }
             } else{
                 isContinuously = false
                 videoView!!.pause()
                 stopPosition = videoView!!.currentPosition
             }
+        }
+
+        mInterstitialAd.setAdListener(object : AdListener() {
+            override fun onAdClosed() {
+                // reschedule
+                mInterstitialAd.loadAd(AdRequest.Builder().build())
+            }
+        })
+
+        imageButtonSecond.setOnClickListener {
+            if (mInterstitialAd.isLoaded) {
+                    mInterstitialAd.show()
+                } else {
+                    Log.d("TAG", "The interstitial wasn't loaded yet.")
+                }
+            }
+
+        imageButtonFourth.setOnClickListener {
+            if (mInterstitialAd.isLoaded) {
+                mInterstitialAd.show()
+            } else {
+                Log.d("TAG", "The interstitial wasn't loaded yet.")
+            }
+        }
+
+        goBack.setOnClickListener{
+            mediaPlayer?.prepare()
+            mediaPlayer?.start()
+            arrowRight?.visibility = VISIBLE
+            horizontalScrollViewComp?.visibility = VISIBLE
+            videoView?.visibility = GONE
+            goBack?.visibility = GONE
+        }
+
+        arrowRight.setOnClickListener{
+            val hola = horizontalScrollViewComp?.getScrollX()
+            val we = hola?.plus(1200)
+            val nonNullableInt: Int = we!!
+            horizontalScrollViewComp?.scrollTo(nonNullableInt, 0)
+        }
+        arrowLeft.setOnClickListener{
+            val hola = horizontalScrollViewComp?.getScrollX()
+            val nonNullableInt: Int = hola!!
+            horizontalScrollViewComp?.scrollTo(-nonNullableInt, 0)
         }
     }
 }
